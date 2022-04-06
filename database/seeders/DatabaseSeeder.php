04b359974a0f611abe118148f571c666d3165b2f
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2022-04-06 15:40:34 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(CatagorySeeder::class);
         $this->call(SubcategorieSeeder::class);
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         
       
 
