2494fbde1d85b5688b7ef727ed2f66d1bbb242b8
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2022-04-06 17:42:38 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -36,14 +36,13 @@ Route::controller(CatagoryController::class)->group(function(){
     // Route::get('softwareCategory', [CatagoryController::class, 'queryCategorySoftware']);
     // Route::get('persernalCategory', [CatagoryController::class, 'queryCategoryPersernal']);
     // Route::get('businessCategory', [CatagoryController::class, 'queryCategoryBusiness']);
-
+    Route::resource('posts', PostController::class);
 });
 
         
 
 // Protect
 Route::middleware('auth:sanctum')->group( function () {
-    Route::resource('posts', PostController::class);
     Route::resource('comment',CommentController::class);
     
 });
